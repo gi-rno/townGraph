625fc381260c2263402c5680141f39e1d7e12272
gi-rno <cjoyalop@montgomerycollege.edu>
2025-11-25 20:42:12 -0500

    Refactor Town class to use Set for adjacent towns

--- Town.java
+++ Town.java
@@ -1,4 +1,5 @@
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 /**@author Christopher Joya Lopez. This class represents Towns(vertices) for our graph.
  * Some towns will be linked through roads(edges), so this class also stores adjacent(next to)
  * towns. You can also compare towns by their corresponding names.
@@ -7,14 +8,35 @@ public class Town implements Comparable<Town>{
 
 	private String townName;
 	
-	private ArrayList<Town> adjTowns = new ArrayList<>();
+	private Set<Town> adjTowns;
+	
+	private int weight;
+	
+	private Town backPath;
 
 	public Town(String name) {
 		
 		townName = name;
+		
+		adjTowns = new HashSet<>();
+		
+		weight = Integer.MAX_VALUE;
+		
+		backPath = null;
 	}
 	
-	public String getTownName() {
+	public Town(Town templateTown) {
+		
+		townName = templateTown.townName;
+		
+		adjTowns = new HashSet<>(templateTown.adjTowns);
+		
+		weight = templateTown.weight;
+		
+		backPath = templateTown.backPath;
+	}
+	
+	public String getName() {
 		
 		return townName;
 	}
@@ -26,27 +48,24 @@ public class Town implements Comparable<Town>{
 		adjTowns.clear();
 	}
 	
-	public Town getAdjacentTown(int index) {
+	public int getWeight() {
 		
-		return adjTowns.get(index);
+		return weight;
 	}
 	
-	public String getAdjacentTowns() {
+	public void setWeight(int weight) {
 		
-		String[] adjTown = new String[adjTowns.size()];
+		this.weight = weight;
+	}
+	
+	public void addAdjTown(Town adjacentTown) {
 		
-		StringBuilder sb = new StringBuilder();
+		adjTowns.add(adjacentTown);
+	}
+	
+	public Set<Town> getAdjacentTowns() {
 		
-		for(int i = 0; i < adjTowns.size(); i++) {
-			
-			adjTown[i] = adjTowns.get(i).getTownName();
-			
-			sb.append(adjTown[i]);
-			
-			sb.append(", ");
-		}
-		
-		return sb.toString();
+		return adjTowns;
 	}
 	
 	public void setAdjacentTowns(Town name) {
@@ -54,18 +73,54 @@ public class Town implements Comparable<Town>{
 		adjTowns.add(name);
 	}
 	
+	public void setBackPath(Town bp) {
+		
+		backPath = bp;
+	}
+	
+	public Town getBackPath() {
+		
+		return backPath;
+	}
+	
 	@Override
 	public String toString() {
 		
-		return "Town name: " + townName + ", \nAdjacent Towns: "
-				+ getAdjacentTowns();
+		return townName;
 	}
 
 	@Override
 	public int compareTo(Town o) {
 		
-		return this.getTownName().compareTo(o.getTownName());
+		return this.getName().compareTo(o.getName());
 	}
 	
+	@Override
+	public boolean equals(Object obj) {
+		
+		if(this == obj) {
+			
+			return true;
+		}
+		
+		if(!(obj instanceof Town)) {
+			
+			return false;
+		}
+		
+		Town other = (Town) obj;
+		
+		return this.townName.equals(other.townName);
+	}
 	
+	@Override
+	public int hashCode() {
+		
+		return townName.hashCode();
+	}
+	
+	public int getMinWtToSource(Town sourceVertex) {
+		
+		return weight;
+	}
 }
